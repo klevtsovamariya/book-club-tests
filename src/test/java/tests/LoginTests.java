9bb6c9ba9acfc1f150d1915df8fc13d000d378d8
svasenkov <aanher@gmail.com>
2026-02-26 18:54:10 +0100

    added login tests

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,75 @@
+package tests;
+
+import models.login.LoginBodyModel;
+import models.login.SuccessfulLoginResponseModel;
+import models.login.WrongCredentialsLoginResponseModel;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTests extends TestBase {
+
+    String username = "qaguru";
+    String password = "qaguru123";
+    String wrongPassword = "qaguru1234";
+
+    @Test
+    public void successfulLoginTest(){
+        LoginBodyModel loginData = new LoginBodyModel(username, password);
+
+        SuccessfulLoginResponseModel loginResponse = given()
+                .log().all()
+                .contentType(JSON)
+                .body(loginData)
+                .basePath("/api/v1")
+                .when()
+                .post("/auth/token/")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/successful_login_response_schema.json"))
+                .body("access", notNullValue())
+                .body("refresh", notNullValue())
+                .extract().as(SuccessfulLoginResponseModel.class);
+
+        String expectedTokenPath = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+        String actualAccess = loginResponse.access();
+        String actualRefresh = loginResponse.refresh();
+
+        assertThat(actualAccess).startsWith(expectedTokenPath);
+        assertThat(actualRefresh).startsWith(expectedTokenPath);
+        assertThat(actualAccess).isNotEqualTo(actualRefresh);
+    }
+
+    @Test
+    public void wrongCredentialsLoginTest(){
+        LoginBodyModel loginData = new LoginBodyModel(username, wrongPassword);
+
+        WrongCredentialsLoginResponseModel loginResponse = given()
+                .log().all()
+                .contentType(JSON)
+                .body(loginData)
+                .basePath("/api/v1")
+                .when()
+                .post("/auth/token/")
+                .then()
+                .log().all()
+                .statusCode(401)
+                .body(matchesJsonSchemaInClasspath(
+                        "schemas/wrong_credentials_login_response_schema.json"))
+                .body("detail", notNullValue())
+                .extract().as(WrongCredentialsLoginResponseModel.class);
+
+        String expectedDetailError = "Invalid username or password.";
+        String actualDetailError = loginResponse.detail();
+
+        assertThat(actualDetailError).isEqualTo(expectedDetailError);
+    }
+
+}
